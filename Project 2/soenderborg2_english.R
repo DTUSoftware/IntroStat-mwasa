9e5451956d96b7c38d31f9bea5ee5fe71d9d9e14
HeroGamers <hero@herogamers.dev>
2022-11-07 01:01:49 +0100

    Add figures

--- Project 2/soenderborg2_english.R
+++ Project 2/soenderborg2_english.R
@@ -89,17 +89,17 @@ ggplot(D_model, aes(G)) +
 ggplot(melt(D_model[variables[1]], id.vars = NULL), aes(x = variable, y = value)) +
   stat_boxplot(geom = 'errorbar') +
   geom_boxplot() +
-  labs(x = "Variable", y = "Heat Consumption (Q) [kW]", title = "Box plot showing heat consumption")
+  labs(x = "Variable", y = "Heat Consumption [kW]", title = "Box plot showing heat consumption")
 
 ggplot(melt(D_model[variables[2]], id.vars = NULL), aes(x = variable, y = value)) +
   stat_boxplot(geom = 'errorbar') +
   geom_boxplot() +
-  labs(x = "Variable", y = "Outdoor Temperature (Ta) [Celsius]", title = "Box plot showing outdoor temperature")
+  labs(x = "Variable", y = "Outdoor Temperature [Celsius]", title = "Box plot showing outdoor temperature")
 
 ggplot(melt(D_model[variables[3]], id.vars = NULL), aes(x = variable, y = value)) +
   stat_boxplot(geom = 'errorbar') +
   geom_boxplot() +
-  labs(x = "Variable", y = "Solar Irradiance (G) [W/m^2]", title = "Box plot showing solar irradiance")
+  labs(x = "Variable", y = "Solar Irradiance [W/m^2]", title = "Box plot showing solar irradiance")
 
 # Summary Table
 Tbl <- apply(D_model[, variables], 2, function(x) {
@@ -129,31 +129,31 @@ summary(fit)
 ###########################################################################
 ## Plots for model validation
 
-fit_df <- data.frame(fitted_values = fit$fitted.values, residuals = fit$residuals,
+D_fit <- data.frame(fitted_values = fit$fitted.values, residuals = fit$residuals,
                      Q = fit$model$Q, Ta = fit$model$Ta, G = fit$model$G)
 
 # Observations against fitted values
-ggplot(fit_df, aes(x = fitted_values, y = Q)) +
+ggplot(D_fit, aes(x = fitted_values, y = Q)) +
   geom_point() +
   geom_smooth(method = lm, se = FALSE) + # Regression line, without confidence interfal
   geom_smooth(linetype = "dashed") +     # Loess method
   labs(x = "Fitted values", y = "Heat Consumption (Q) [kW]", title = "Scatter plot for observations against fitted values")
 
 # Residuals against each of the explanatory variables
-ggplot(fit_df, aes(x = Ta, y = residuals)) +
+ggplot(D_fit, aes(x = Ta, y = residuals)) +
   geom_point() +
   geom_smooth(method = lm, se = FALSE) + # Regression line, without confidence interfal
   geom_smooth(linetype = "dashed") +     # Loess method
   labs(x = "Outdoor Temperature (Ta) [Celsius]", y = "Residuals", title = "Scatter plot for residuals against outdoor temperature")
 
-ggplot(fit_df, aes(x = G, y = residuals)) +
+ggplot(D_fit, aes(x = G, y = residuals)) +
   geom_point() +
   geom_smooth(method = lm, se = FALSE) + # Regression line, without confidence interfal
   geom_smooth(linetype = "dashed") +     # Loess method
   labs(x = "Solar Irradiance (G) [W/m^2]", y = "Residuals", title = "Scatter plot for residuals against solar irradiance")
 
 # Residuals against fitted values
-ggplot(fit_df, aes(x = fitted_values, y = residuals)) +
+ggplot(D_fit, aes(x = fitted_values, y = residuals)) +
   geom_point() +
   geom_smooth(method = lm, se = FALSE) + # Regression line, without confidence interfal
   geom_smooth(linetype = "dashed") +     # Loess method
@@ -161,15 +161,45 @@ ggplot(fit_df, aes(x = fitted_values, y = residuals)) +
 
 # Normal QQ-plot of the residuals
 qqnorm.wally <- function(x, y, ...) { qqnorm(y, ...); qqline(y, ...) }
-## qq-plot of daily heat consumption (House 1)
-wallyplot(fit$residuals, FUN = qqnorm.wally, ylab = "Residuals", xlab = "Z-scores", main = "", hide = FALSE) # Multiple (simulated) Q-Q Plots for House 1
+## qq-plot of the residuals
+wallyplot(D_fit$residuals, FUN = qqnorm.wally, ylab = "Residuals", xlab = "Z-scores", main = "", hide = FALSE) # Multiple (simulated) Q-Q Plots for House 1
 
 
 ###########################################################################
+## Confidence intervals for the model coefficients
+
+## CI for the heat consumption, manually
+n <- length(D_fit$Q)
+beta_1 <- unname(fit$coefficients[2])
+conf_level <- 0.95
+a <- 1 - conf_level
+t_0.975 <- qt(1 - a / 2, n - (2e-16 + 1)) # didn't want to find out where to get this manually
+c(beta_1 - t_0.975 * 0.0058260, unname(fit$coefficients[2]) + t_0.975 * 0.0058260) # and this is also just manually put in
 
 # Confidence intervals for the model coefficients
 confint(fit, level = 0.95)
 
+###########################################################################
+## Hypothesis
+
+# test statistic
+t_obs <- (beta_1 - (-0.25))/0.0058260
+
+## Test statistic distribution
+x <- seq(-4, 4, by = 0.01)
+y <- dt(x, df = n - 2)
+
+ggplot(data.frame(x = x, y = y), aes(x = x, y = y)) +
+  geom_line() +
+  annotate(geom = "text", x = 2.0, y = 0.2, label = "Black: t(574)", col = "black") +
+  geom_polygon(data = data.frame(x = c(x[x >= t_0.975], max(x), t_0.975), y = c(y[x >= t_0.975], 0, 0)), aes(x = x, y = y), fill = "red", alpha = 0.75) +
+  annotate(geom = "text", x = t_0.975, y = -0.01, label = "t_0.975", col = "red") +
+  geom_polygon(data = data.frame(x = c(min(x), x[x <= -t_0.975], -t_0.975), y = c(y[x <= -t_0.975], 0, 0)), aes(x = x, y = y), fill = "red", alpha = 0.75) +
+  annotate(geom = "text", x = -t_0.975, y = -0.01, label = "-t_0.975", col = "red") +
+  labs(y = "Density(x)", title = "Distribution of the test statistic (n-2 (574) degrees of freedom)")
+
+
+###########################################################################
 
 # Make dataset for validation
 D_test <- subset(D, (t == "2008-12-06" & houseId == 3) |
